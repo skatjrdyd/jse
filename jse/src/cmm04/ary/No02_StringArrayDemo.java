eabd8784cad28d1aa2e0a801ad3a4e55d4a12ab2
john <Administrator@MSDN-SPECIAL>
2015-05-14 11:54:18 +0900

    array

--- jse/src/cmm04/ary/No02_StringArrayDemo.java
+++ jse/src/cmm04/ary/No02_StringArrayDemo.java
@@ -4,8 +4,8 @@ public class No02_StringArrayDemo {
 	public static void main(String[] args) {
 		String[] juso = {"서울","종로","광화문"}; // juso뒤에 juso[]이렇게 해도 됨
 		
-		for(int i = 0; i < 3; i++){
-			System.out.println("배열 출력 " + juso[i]);
+		for(int i = 0; i < juso.length; i++){     // int < 3 해도 되는데 만약 숫자가 커지면 세기 힘드니, 배열 이름 치고 . 치고 length 선택
+			System.out.println("주소는 " + juso[i]);
 		}
 		
 		/*juso[0] = "서울";
